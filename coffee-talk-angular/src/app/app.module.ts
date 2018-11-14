80ba4a533aa4407596877a13f84bb375d0067ba1
Joshua Membreno <membrenojoshua@gmail.com>
2018-11-13 22:39:53 -0600

    added home component and routing

--- coffee-talk-angular/src/app/app.module.ts
+++ coffee-talk-angular/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },  
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true }),
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
