017f9cc798fd5f6d9176f97d8e4a7870b9a01cae
Joshua Membreno <38535134+jmembreno1029@users.noreply.github.com>
2018-11-16 12:29:13 -0600

    added menu componement, routing and cleaned up default

--- CoffeeTalk/ClientApp/src/app/app.module.ts
+++ CoffeeTalk/ClientApp/src/app/app.module.ts
@@ -9,14 +9,15 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,8 +25,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'menu', component: MenuComponent },
     ])
   ],
   providers: [],
