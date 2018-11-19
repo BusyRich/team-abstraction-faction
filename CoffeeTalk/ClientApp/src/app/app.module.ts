bc4c6e0e8e1231ec2377ee54a7ffbcf93eac3b86
Joshua Membreno <38535134+jmembreno1029@users.noreply.github.com>
2018-11-18 22:06:00 -0600

    Added question base for menu and derived classes for text box and dropdown questions

--- CoffeeTalk/ClientApp/src/app/app.module.ts
+++ CoffeeTalk/ClientApp/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { MenuComponent } from './menu/menu.component';
 
@@ -27,7 +26,8 @@ import { MenuComponent } from './menu/menu.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'menu', component: MenuComponent },
-    ])
+    ]),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
