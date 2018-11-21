cd5097652ea1879e0a02443c56e1ab17d2869d1b
Joshua Membreno <38535134+jmembreno1029@users.noreply.github.com>
2018-11-21 09:45:36 -0600

    removed previously added files and component for to implement a different form approach

--- CoffeeTalk/ClientApp/src/app/app.module.ts
+++ CoffeeTalk/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { MenuComponent } from './menu/menu.component';
-import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-fo
     HomeComponent,
     FetchDataComponent,
     MenuComponent,
-    DynamicFormQuestionComponent
+    
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -29,7 +29,6 @@ import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-fo
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'menu', component: MenuComponent },
     ]),
-    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CoffeeTalk/ClientApp/src/app/menu/menu.component.ts
+++ CoffeeTalk/ClientApp/src/app/menu/menu.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
-})
-export class MenuComponent implements OnInit {
+  providers: []
 
-  constructor() { }
+})
+export class MenuComponent {
+  
+  constructor() {}
 
   ngOnInit() {
   }
